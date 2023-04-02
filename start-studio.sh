39461829c707235a9f61b71e28c1c2897bad81f2
SalatielGenol <salatiel@genol.es>
2023-04-03 00:26:23 +0200

    Fixed directory path slash

--- start-studio.sh
+++ start-studio.sh
@@ -2,11 +2,11 @@
 
 base_dir=`pwd`
 
-export STUDIO_PROPERTIES=$base_dir\studio.properties
-export ANDROID_HOME=$base_dir\sdk
-export ANDROID_USER_HOME=$base_dir\.android
-export ANDROID_EMULATOR_HOME=$base_dir\.android_machines
-export ANDROID_AVD_HOME=$base_dir\.android_machines\avd
-export STUDIO_GRADLE_JDK=$base_dir\sw_files\jre\
-export GRADLE_USER_HOME=$base_dir\.gradle
-sh $base_dir\sw_files\bin\studio.sh
+export STUDIO_PROPERTIES=$base_dir/studio.properties
+export ANDROID_HOME=$base_dir/sdk
+export ANDROID_USER_HOME=$base_dir/.android
+export ANDROID_EMULATOR_HOME=$base_dir/.android_machines
+export ANDROID_AVD_HOME=$base_dir/.android_machines/avd
+export STUDIO_GRADLE_JDK=$base_dir/sw_files/jre
+export GRADLE_USER_HOME=$base_dir/.gradle
+sh $base_dir/sw_files/bin/studio.sh
